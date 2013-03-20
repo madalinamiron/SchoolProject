5d0c4a69b87198b7170b7f755be73e95ee6addd4
Madalina Miron <madalina.miron@3pillarglobal.com>
2013-03-20 13:52:52 +0200

    second commit

---
+++ Exercise3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Exercise3
+//
+//  Created by Madalina Miron on 12/18/12.
+//  Copyright (c) 2012 Madalina Miron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
